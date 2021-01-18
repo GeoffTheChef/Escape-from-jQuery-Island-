1af870c9cf89041cda4f0166c410b6817f800990
GeoffTheChef <57178313+GeoffTheChef@users.noreply.github.com>
2021-01-18 09:11:58 -0500

    This comes after a seemingly successful "npm run build" and the creation of the dsitribution bundle of the code by Webpack

---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+console.log("it works!");
+//# sourceMappingURL=bundle.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mode: process.env.NODE_ENV || 'production',
     devtool: 'source-map',
-    entry: '.js/test.js', // I will have to look at this more in depth to get it right, but good enough for now... 
+    entry: './js/test.js', // I will have to look at this more in depth to get it right, but good enough for now... 
     output: {
         filename: 'bundle.js',
     },
