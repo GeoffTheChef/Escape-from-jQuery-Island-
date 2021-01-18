df6882508c389f4ff204721369275d13c08ad0ea
GeoffTheChef <57178313+GeoffTheChef@users.noreply.github.com>
2021-01-18 13:03:12 -0500

    Tried to add Babel dev dependecies

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack'); 
+const nodeEnv = process.env.NODE_ENV || 'production';
+
 module.exports = {
     mode: process.env.NODE_ENV || 'production',
     devtool: 'source-map',
