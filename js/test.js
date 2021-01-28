84fc5d68ee8372b502b7296db6dcaa763a0a9152
GeoffTheChef <57178313+GeoffTheChef@users.noreply.github.com>
2021-01-28 14:30:54 -0500

    This is the last commit of how this program working being purely along my line of thought. Past this point I am incorporating a few suggestions in order to pick up some new coding constructs. The next part of the project tackles jumping from one scene (an array of textBlocks) to another, some debugging may be required but the cycling and iterating through text blocks using spacebar has some decent logic there.

--- js/test.js
+++ js/test.js
@@ -1,73 +1,103 @@
-// Escape from jQuery Island 
-// import $ from "jquery"; 
+// JS File for text adventure game
 
-
-// First keycode ="space"
-
-console.log('it works, so do it up, and do it up again'); 
-
-// Create a variables for the different spots where text can go, and select those using 
-// document.querySelector('.nameOfTextSpotToInputTextInto ');
+// DOM Interaction 
 
 const titleText = document.querySelector('#titleText'); 
 const chapterText = document.querySelector('#chapterText');
 const storyText = document.querySelector('#storyText');
-const promptText = document.querySelector('#promptText')
+const promptText = document.querySelector('#promptText'); 
+
+// Selecting optionButtons in HTML 
+//const firstButton = document.querySelector('.optionButton')
+// const secondButton = document.querySelector('.optionButton2')
+
+// Data 
+
+// Decisions
+const decision1 = `<button id="optionA"> Option A1 </button> or <button id="optionB> Option B1 </button>`;
+const decision2 = `<button> Option A2 </button> or <button> Option B2 </button>`;
+const decision3 = `<button> Option A3 </button> or <button> Option B3 </button>`;
+
+// Chapter 1, Scene 1, text blocks
+const c1s1t1 = `<p> Chapter 1 Scene 1 Text 1 </p>`;
+const c1s1t2 = `<p> Chapter 1 Scene 1 Text 2 </p>`;
+const c1s1t3 = `<p> Chapter 1 Scene 1 Text 3 </p>`;
+const c1s1t4 = decision1;
+ 
+// Chapter 1, Scene 2, text blocks
+const c1s2t1 = `<p> c1s2t1 </p>`;
+const c1s2t2 = `<p> c1s2t2 </p>`;
+const c1s2t3 = `<p> c1s2t3 </p>`;
+const c1s2t4 = decision2; 
+ 
+// Chapter 1, Scene 3, text blocks
+const c1s3t1 = `<p> c1s2t1 </p>`;
+const c1s3t2 = `<p> c1s2t2 </p>`;
+const c1s3t3 = `<p> c1s2t3 </p>`;
+const c1s3t4 = decision3; 
+  
+// Chapter 1, Scenes
+const scene1 = [c1s1t1, c1s1t2, c1s1t3, c1s1t4];
+const scene2 = [c1s2t1, c1s2t2, c1s2t3, c1s2t4];
+const scene3 = [c1s3t1, c1s3t2, c1s3t3, c1s2t4]; 
+
+// Chapter 1 
+const chapter1 = [scene1, scene2, scene3]; 
+
+// Now we are modelling out the functionality 
+
+// Gives you the length of a chapter 
+console.log(chapter1.length);  
+
+// This gets you the first scene of 3 arrays
+console.log(chapter1[1]);  
+
+// Successfully logged 3 arrays to console.
+console.log(chapter1); 
+
+// This variable can search through the indexes in the arrays
+let storyCoordinates = chapter1[sceneIndex][textBlockIndex];
+console.log(storyCoordinates); 
+
+// This is an on.Click event which logs something to the console when you click the button for 
+// option A or option B 
+
+// const optionAButton = document.querySelector('#optionA');
+// optionAButton.addEventListener('click', function() {
+//     console.log('You clicked the button for optionA')
+// })
+
+// const optionBButton = document.querySelector('#optionB'); 
+// optionBButton.addEventListener('click', function() {
+//     console.log('You clicked the button for optionB')
+// })
+
+let currentScene; 
+let storyIndex; 
+let sceneIndex;
+let textBlockIndex; 
+
+storyIndex = 0; 
+sceneIndex = 0; 
+textBlockIndex = -1; 
+
+console.log(chapter1[sceneIndex][textBlockIndex]); // This gets you the first text block
 
 // Add an event listener to the window that registers an event based on a "keydown" input
 
 window.addEventListener('keydown', handleKeyDown); 
 
-// create a function which, if the event.key === "Space", which first, logs the new
-// text to the console, and then switches the .textContent in the location selected 
-// by the previously declared variable with some `new text` 
-
 function handleKeyDown(event) {
     if (event.code === "Space") {
-        console.log('You just hit space')
-        storyText.innerHTML = `<p> You just had a nightmare about something impossible ... </p>
-        <p> Could Java and JavaScript really be the same thing? </p>
-        <p> No, that was just a dream. </p> 
-        <p> You leave your hotel lobby. </p> 
-        </br> 
-        `;
-        promptText.innerHTML = `
-         <button class="optionButton">
-            <p> Take the stairs</p>
-         </button>
-
-         <button class="optionButton">
-         <p> Take the elevator</p>
-         </button>  `
-         
+        textBlockIndex = textBlockIndex + 1;
+        console.log(chapter1[sceneIndex][textBlockIndex])
+        storyText.innerHTML = chapter1[sceneIndex][textBlockIndex]; // This gets you the first text block // This is close but not exactly, the innerHTML should be accessing the result of the counter being at a certain place 
+        //This is where the next text block should be;
+        //console.log(chapter1[1][1])
+        // promptText.innerHTML = scene1[2] // Sometimes there will be a prompt, sometimes there will not be
     }
 }
 
-// // Now to get the buttons to function
-
-//const firstButton = document.querySelector('.optionButton')
-// const secondButton = document.querySelector('.optionButton2') 
-
-// // Keep in mind that you can put more things into this function later 
-
- //firstButton.addEventListener('click', function() {
-     //console.log('You just hit the button for option 1')
-   // } 
-//     storyText.innerHTML = `<p> Great Job </p>
-//     <p> No, that was just a dream. </p> 
-//     <p> You leave your hotel lobby. </p> 
-//     </br>  
-//     `
-//     promptText.innerHTML = `
-//     <button class="optionButton">
-//     <p> Take the stairs</p>
-//     </button>
-
-//     <button class="optionButton">
-//     <p> Take the elevator</p>
-//     </button>
-//     `;`;   
-// })
 
 
 
@@ -78,15 +108,4 @@ function handleKeyDown(event) {
 
 
 
-// import $ from "jquery";
 
-// var settings = { 
-//     "url": "https://pokeapi.co/api/v2/pokemon/bellsprout/",
-//     "method": "GET",
-//     "timeout": 0, 
-// }; 
-
-// $.ajax(settings).done(function(response) {
-//     console.log(response);
-//     $(".pokemonData").append("<p>" + JSON.stringify(response) + "</p>"); 
-// })
